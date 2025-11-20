9f2b80e56d411db7cbc3907b908f6d37929e243d
SPS-JPNPahang <sektorpengurusansekolahpahang@gmail.com>
2025-11-20 18:12:39 +0800

    Update slides.js

--- js/slides.js
+++ js/slides.js
@@ -22,8 +22,8 @@ const slidesData = [
     },
     {
         image: 'images/slider/slide2.jpg',
-        title: 'Kita Bersama',
-        description: '4C'
+        title: 'SEMAKAN TAWARAN TINGKATAN ENAM TAHUN 2025',
+        description: 'https://sst6.moe.gov.my/semakant6/'
     },
     {
         image: 'images/slider/slide3.jpg',
@@ -139,4 +139,5 @@ const slidesData = [
         description: 'Sentiasa bersedia membantu anda'
     }
 ];
+
 */
